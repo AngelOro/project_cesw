00e43febb0cf39d68d0cda24e12194b97cc379ba
Esteban Olaya <juanesolayalopez@gmail.com>
2020-06-04 20:42:17 -0500

    Se añade modal en conductores

--- sotransiv/src/components/ConductorContent.jsx
+++ sotransiv/src/components/ConductorContent.jsx
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import "../styles/VehicleContent.css";
 import { link, Link } from "react-router-dom";
 import Axios from "axios";
+import Modal from "react-awesome-modal";
+//import "../styles/FormRegister.css";
+
+
 
 class ConductorContent extends Component {
   constructor(props) {
@@ -10,8 +14,20 @@ class ConductorContent extends Component {
       loading: true,
       error: null,
       data: {},
-      dataVehicles: {},
+      isShowing: false,
     };
+  }
+
+  openModal() {
+    this.setState({
+      visible: true,
+    });
+  }
+
+  closeModal() {
+    this.setState({
+      visible: false,
+    });
   }
 
   _fetchData() {
@@ -55,30 +71,110 @@ class ConductorContent extends Component {
         <div className="row" id="row-container">
           <div className="col-md-6">
             <div className="form-row" id="form-input">
-              <input className="input-search" type="text" placeholder="Buscar" />
+              <input
+                className="input-search"
+                type="text"
+                placeholder="Buscar"
+              />
               <a className="nav-link" href="#">
-              <i className="icon ion-md-search lead mr-2"></i>
+                <i className="icon ion-md-search lead mr-2"></i>
               </a>
             </div>
           </div>
           <div className="col-md-6 btn-new">
-            <Link to="/RegisterConduct">
-              <button
-                type="button"
-                className="btn-3 btn-primary "
-                id="btn-search"
-              >
-                + Nuevo
-              </button>
-            </Link>
+            <button
+              type="button"
+              className="btn-3 btn-primary "
+              id="btn-search"
+              value="Open"
+              onClick={() => this.openModal()}
+            >
+              + Nuevo
+            </button>
           </div>
         </div>
+
+        <section>
+          <Modal
+            visible={this.state.visible}
+            width="950"
+            height="700"
+            effect="fadeInUp"
+            onClickAway={() => this.closeModal()}
+          >
+            <div>
+              <div className="form-vehiculo">
+                <h3 className="form-title">Registrar conductor</h3>
+                <form className="formConduct">
+                  <div className="form-row">
+                    <div className="col-md-4 leftSeparator">
+                      <div className="form-group">
+                        <label>Identificacion</label>
+                        <input type="text" className="form-control" />
+                      </div>
+                      <div className="form-group">
+                        <label>Primer apellido</label>
+                        <input type="text" className="form-control" />
+                      </div>
+                      <div className="form-group">
+                        <label>Telefono</label>
+                        <input type="text" className="form-control" />
+                      </div>
+                      <div className="form-group">
+                        <label>Licencia de conduccion</label>
+                        <input type="text" className="form-control" />
+                      </div>
+                      <div className="form-group">
+                        <label class="container">
+                          <input type="checkbox" /> Acepto de terminos y
+                          condiciones
+                          <span class="checkmark"></span>
+                        </label>
+                      </div>
+                    </div>
+
+                    <div className="col-md-4 leftSeparator">
+                      <div className="form-group">
+                        <label>Nombre</label>
+                        <input type="text" className="form-control" />
+                      </div>
+                      <div className="form-group">
+                        <label>Segundo apellido</label>
+                        <input type="text" className="form-control" />
+                      </div>
+                      <div className="form-group">
+                        <label>Vehiculo asignado</label>
+                        <select
+                          className="form-control"
+                          name="vehiculo_asignado"
+                        ></select>
+                      </div>
+                      <div className="form-group">
+                        <label>Fecha de nacimiento</label>
+                        <input className="form-control" type="date" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="form-group col-md-4">
+                    <button
+                      type="submit"
+                      className="btn-primary btn-formConduct"
+                      id="btn-conduct"
+                    >
+                      Registrar conductor
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </Modal>
+        </section>
 
         <table className="table table-striped">
           <thead>
             <tr>
               <th scope="col">Identificacion</th>
-              <th scope="col" >Nombre</th>
+              <th scope="col">Nombre</th>
               <th scope="col">Telefono</th>
               <th scope="col">Vehiculo Asignado</th>
               <th scope="col">Acciones</th>
@@ -89,7 +185,13 @@ class ConductorContent extends Component {
             <tbody>
               <tr>
                 <td>{character.identificacion}</td>
-                <td>{character.nombre + ' ' + character.primer_apellido + ' ' + character.segundo_apellido}</td>
+                <td>
+                  {character.nombre +
+                    " " +
+                    character.primer_apellido +
+                    " " +
+                    character.segundo_apellido}
+                </td>
                 <td>{character.telefono}</td>
                 <td></td>
                 <td>
