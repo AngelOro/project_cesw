277d66fc97350cfaac795789bb52a145a9463a46
edwaralej12 <edwaralejandro595@gmail.com>
2020-06-15 16:03:41 -0500

    se publica en pages

--- sotransiv/src/components/UserContent.jsx
+++ sotransiv/src/components/UserContent.jsx
@@ -15,7 +15,7 @@ class userContent extends Component {
      
     
       componentDidMount() {
-        axios.get('http://192.168.1.73:3001/user/listUser')
+        axios.get('http://192.168.1.67:3001/user/listUser')
         .then(res => {
             if(res.data.success){
                 const data = res.data.data
