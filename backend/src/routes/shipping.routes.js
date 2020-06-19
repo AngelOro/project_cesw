5c8662904cbcc031979f6ce0080cacfdd09b5e00
edwaralej12 <edwaralejandro595@gmail.com>
2020-06-19 18:49:50 -0500

    actualizacion

--- backend/src/routes/shipping.routes.js
+++ backend/src/routes/shipping.routes.js
@@ -2,8 +2,13 @@ const express = require('express')
 const router = express.Router()
 const controllerShipping = require('../controllers/shipping.controller')
 
-router.get('/testData', controllerShipping.testdata);
-router.get('/listUser', controllerShipping.list)
+
+router.get('/', controllerShipping.getVehicles);
+
+// router.get('/', (req, res) => {
+//     res.json({status: "Listando Vehiculos"})
+// })
+
 
 
 module.exports = router;
--- backend/src/routes/vehicle.routes.js
+++ backend/src/routes/vehicle.routes.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const controllerVehicle = require('../controllers/vehicle.controller')
-const controllerUser = require('../controllers/users.controller')
-const controllerShipping = require('../controllers/shipping.controller')
+
 
 router.get('/', controllerVehicle.getVehicles);
 
